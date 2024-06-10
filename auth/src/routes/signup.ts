fef905fc06ed9e4efe71f99ae9a191960fa26b1f
Mohab2000 <mohabahmed20005@gmail.com>
2024-06-10 21:49:22 +0300

    Error handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+
 const router = express.Router();
 
 router.post(
@@ -14,9 +17,10 @@ router.post(
   (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      throw new Error("Invalid email or password");
+      throw new RequestValidationError(errors.array());
     }
     console.log("Creating a user...");
+    throw new DatabaseConnectionError();
     res.send({});
   }
 );
