f6c28ef58765ba69b7f04c42a8fb352b474e6972
Mohab2000 <mohabahmed20005@gmail.com>
2024-06-11 17:26:13 +0300

    Error handling

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,19 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
   constructor(public errors: ValidationError[]) {
-    super();
+    super("Invalid request parameters");
     //Only beccause we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
+  serializeErrors() {
+    return this.errors.map((err) => {
+      if (err.type === "field") {
+        return { message: err.msg, field: err.path };
+      }
+      return { message: err.msg };
+    });
+  }
 }
-
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error,
@@ -8,23 +7,10 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map((error) => {
-      if (error.type === "field") {
-        return { message: error.msg, field: error.path };
-      }
-    });
-    return res.status(400).send({ errors: formattedErrors });
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(500).send({
-      errors: [
-        {
-          message: err.reason,
-        },
-      ],
-    });
-  }
+
   res.status(400).send({
     errors: [{ message: "Something went wrong" }],
   });
