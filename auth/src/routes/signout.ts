fcffcea656e9fddd0b716e7821102175254a6fee
Mohab2000 <mohabahmed20005@gmail.com>
2024-06-26 17:46:27 +0300

    Tests for auth Service

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
   req.session = null;
-  res.send({});
+  res.send(200);
 });
 
 export { router as signoutRouter };
