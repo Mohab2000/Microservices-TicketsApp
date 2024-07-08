6390815d1b0fdd4b8023c94203529ec8f08c77e0
Mohab2000 <mohabahmed20005@gmail.com>
2024-07-08 15:32:58 +0300

    test

--- client/pages/banana.js
+++
@@ -1,5 +0,0 @@
-const banana = () => {
-    return <h1>Banana Page</h1>;
-}
-
-export default banana
--- client/pages/index.js
+++
@@ -1,5 +0,0 @@
-const Landing = () => {
-    return <h1>Landing Page</h1>;
-  };
-   
-  export default Landing;
