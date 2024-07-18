91ddedefb597994834e50bbe396be701d09a9265
Mohab2000 <mohabahmed20005@gmail.com>
2024-07-18 13:57:39 +0300

    Auth

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import Router from "next/router";
 import useRequest from "../../hooks/use-request";
 export default () => {
   const [email, setEmail] = useState("");
@@ -11,10 +11,11 @@ export default () => {
       email,
       password,
     },
+    onSuccess: () => Router.push("/"),
   });
   const onSubmit = async (event) => {
     event.preventDefault();
-    doRequest();
+    await doRequest();
   };
   return (
     <form onSubmit={onSubmit}>
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,18 @@
-const Landing = () => {
-    return <h1>Landing Page</h1>;
-  };
-   
-  export default Landing;
+import axios from "axios";
+
+//EXECUTED ON THE CLIENT
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  // axios.get("/api/users/currentuser").catch((err) => {
+  //   console.log(err.message);
+  // });
+
+  return <h1>Landing Page</h1>;
+};
+
+//EXECUTED ON THE SERVER
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get("/api/users/currentuser");
+  return response.data;
+};
+export default LandingPage;
