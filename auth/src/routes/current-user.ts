7bac36d1891b3cb9624f5b94bc98aa66a9079ce0
Mohab2000 <mohabahmed20005@gmail.com>
2024-07-25 16:47:19 +0300

    common service added

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { currentUser } from "../middlewares/current-user";
+import { currentUser } from "@tickets-mohab/common";
 
 const router = express.Router();
 
