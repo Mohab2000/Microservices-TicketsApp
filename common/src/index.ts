136d27a36a632feeb4cc8f04e76b7f1756c5f916
Mohab2000 <mohabahmed20005@gmail.com>
2024-07-25 15:59:18 +0300

    additonal config

---
+++ common/src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  red: 10,
+  blue: 1110,
+  green: 1220,
+};
+
+console.log(color);
+
+export default color;
