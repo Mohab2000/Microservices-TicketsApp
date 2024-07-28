114c8dff853181bedaefe48bc2784c5e32ace8a2
Mohab2000 <mohabahmed20005@gmail.com>
2024-07-28 15:59:38 +0300

    fix typo

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -7,7 +7,7 @@ const start = async () => {
   }
 
   if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI be defined");
+    throw new Error("MONGO_URI must be defined");
   }
   try {
     await mongoose.connect(process.env.MONGO_URI);
