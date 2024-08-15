beda79d5d2c96d70c6c785cd5dceba759cf2cb27
Mohab2000 <mohabahmed20005@gmail.com>
2024-08-15 12:19:37 +0300

    Test cases done for ticket

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/tickets";
 
 it("has a route handler listening to /api/tickets for post request", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -10,6 +11,7 @@ it("has a route handler listening to /api/tickets for post request", async () =>
 it("can only be accessed if the user is signed in", async () => {
   const response = await request(app).post("/api/tickets").send({}).expect(401);
 });
+
 it("returns a status other than 401 if the user is signed in", async () => {
   const response = await request(app)
     .post("/api/tickets")
@@ -25,16 +27,6 @@ describe("Invalid title tests", () => {
       .set("Cookie", signin())
       .send({
         title: "",
-        price: 10,
-      })
-      .expect(400);
-  });
-
-  it("returns an error if an invalid title is provided (missing title)", async () => {
-    await request(app)
-      .post("/api/tickets")
-      .set("Cookie", signin())
-      .send({
         price: 10,
       })
       .expect(400);
@@ -59,13 +51,22 @@ describe("Invalid price tests", () => {
       .expect(400);
   });
 });
+
 it("creates a ticket with valid inputs", async () => {
   //add a check to make sure that the ticket is saved to database.
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+  const title = "new title";
   await request(app)
     .post("/api/tickets")
+    .set("Cookie", global.signin())
     .send({
-      title: "new title",
+      title,
       price: 20,
     })
     .expect(201);
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(20);
+  expect(tickets[0].title).toEqual(title);
 });
