541df7c17df02837d7ac43ba7923ac0022c63243
Mohab2000 <mohabahmed20005@gmail.com>
2024-08-26 15:36:39 +0300

    Listner and Publisher

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,9 +1,37 @@
-import nats from "node-nats-streaming";
+import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
-const stan = nats.connect("ticketing", "123", {
+console.clear();
+
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Listener Connected to NATS Successfully");
+
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
+  });
+
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+    options
+  );
+
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === "string") {
+      console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`);
+    }
+    msg.ack();
+  });
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
