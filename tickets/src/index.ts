91dee8c3850f1755ca616f8b607a38ef4c3b7c05
Mohab2000 <mohabahmed20005@gmail.com>
2024-09-22 15:53:09 +0300

    Ticket listener

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -13,6 +13,13 @@ const start = async () => {
   }
   try {
     await natsWrapper.connect('ticketing', 'laks' ,  'http://nats-srv:4222' )
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+    
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (err) {
